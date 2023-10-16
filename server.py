65cb519518a00757ee25a071a356706ab09e3f5e
oyhoyhk <oyhoyhk@naver.com>
2023-10-16 22:59:04 +0900

    import sqlalchemy

--- server.py
+++ server.py
@@ -15,6 +15,19 @@ from starlette.middleware.trustedhost import TrustedHostMiddleware
 from starlette.responses import FileResponse, RedirectResponse
 from datetime import datetime, timedelta
 import shutil
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 
 script_path = os.path.join(os.path.dirname(__file__), "scripts")
 sys.path.append(script_path)
